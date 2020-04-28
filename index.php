bcd6821cd2455faf13424e816a9597ddec29d42a
wandy1989 <64448011+wandy1989@users.noreply.github.com>
2020-04-28 02:05:09 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+ <title>Generate kuota tri</title>
+</head>
+<body>
+<h2><font face='helvetica'>Generate Kuota Tri..</font></h2>
+<p><font face='arial' color='red' size='8'> GUNAKAN DENGAN BIJAK</font></p>
+ <form method = 'post' action= "index.php">
+  <table>
+   <tr>
+    <td>Nomer</td>   
+    <td><input type='text' placeholder='089xxxxxxxxx' name='nomertri'></td>
+    <td></td>
+   </tr>
+   <tr>
+    <td></td>
+    <td><input type='submit' name='button1' value='Kirim Kuota'></td>
+    <td></td>
+   </tr>
+  </table>  
+ </form>
+
+<p>Cara pakai: </p>
+
+<ul>
+  <li>Pastiin lu pernah nukerin kuota bonstri <b>(bukan yang singapore)</b></li>
+  <li>Akses web ini pakai HP</li>
+  <li>Pastiin pakai browser (chrome atau yang lain) dan <b>aktifin data Tri</b></li>
+  <li>buka <a href="https://internet.tri.co.id" target='_blank'>bima.tri.co.id</a> dan pastiin <b>lu udah login via web</b></li>
+  <li>Masukin nomer lu. Teken 'Kirim Kuota'</li>
+</ul>
+<h1>GUA SENGAJA GA PAKE OTP <font color='red' face='arial'>BIAR GA LU JUALIN!</font></h1>
+<h2>Gausah banyak-banyak, bagi sama yang lain! lagi krisis! biar web-nya kaga down</h2>
+<h2><font color='orange'>Resiko tanggung sendiri</font></h2>
+<p>by Anonim17 :)</p>
+ <?php include "fire.php";?>
+</body>
+</html>
